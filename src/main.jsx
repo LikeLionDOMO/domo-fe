1abe6843f77601f82062dc8c16b34e7593534436
seong su kim <know.warehouse02@gmail.com>
2025-08-12 22:14:52 +0900

    home import 경로 수정

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Header from './layout/Header.jsx';
 import Footer from './layout/Footer.jsx';
-import Home from './component/Home.jsx';
+import Home from './page/Home.jsx';
 import Benefix from './page/Benefix.jsx';
 
 createRoot(document.getElementById('root')).render(
