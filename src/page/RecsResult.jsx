3f137d93531f008f67eec4a0ff41a3af9d124e38
seong su kim <know.warehouse02@gmail.com>
2025-08-17 18:41:46 +0900

    if문 스타일링 추가

--- src/page/RecsResult.jsx
+++ src/page/RecsResult.jsx
@@ -10,49 +10,54 @@ const RecsResult = () => {
 
   if (recommendations.length === 0) {
     return (
-      <div>
-        <p>추천 정보를 불러오지 못했습니다.</p>
-        <button onClick={() => nav('/recs/info')}>다시 추천받기</button>
-      </div>
+      <>
+        <div className="recsResult_fallback">
+          <p>추천 정보를 불러오지 못했습니다.</p>
+          <button onClick={() => nav('/recs/info')}>다시 추천받기</button>
+        </div>
+      </>
     );
   }
 
   return (
-    <section className="recsResultPage">
-      <div className="recsResult_list">
-        <div className="list_header">
-          <p className="list_header_title">
-            오늘의 놀거리를
-            <br />
-            모아왔어요!
-          </p>
-          <button className="list_header_backBtn" onClick={() => nav(-1)}>
-            되돌리기
-          </button>
+    <>
+      <section className="recsResultPage">
+        <div className="recsResult_list">
+          <div className="list_header">
+            <p className="list_header_title">
+              오늘의 놀거리를
+              <br />
+              모아왔어요!
+            </p>
+            <button className="list_header_backBtn" onClick={() => nav(-1)}>
+              되돌리기
+            </button>
+          </div>
+          <ul className="list_items">
+            {recommendations.map((rec, index) => (
+              <li key={rec.id} className="list_item">
+                <div className="item_number">{index + 1}</div>
+                <div className="item_info">
+                  <p className="item_name">{rec.name}</p>
+                  <p className="item_address">{rec.address}</p>
+                  <span className="item_benefit">{rec.benefit}</span>
+                </div>
+                <div className="item_dots">...</div>
+              </li>
+            ))}
+          </ul>
+          <div className="list_footer">
+            <BoxButton bgColor="--main-color" height="60px">
+              결정하기
+            </BoxButton>
+          </div>
         </div>
-        <ul className="list_items">
-          {recommendations.map((rec, index) => (
-            <li key={rec.id} className="list_item">
-              <div className="item_number">{index + 1}</div>
-              <div className="item_info">
-                <p className="item_name">{rec.name}</p>
-                <p className="item_address">{rec.address}</p>
-                <span className="item_benefit">{rec.benefit}</span>
-              </div>
-              <div className="item_dots">...</div>
-            </li>
-          ))}
-        </ul>
-        <div className="list_footer">
-          <BoxButton bgColor="--main-color" height="60px">
-            결정하기
-          </BoxButton>
+        {/* naver map */}
+        <div className="recsResult_map">
+          <NaverMap recommendations={recommendations} />
         </div>
-      </div>
-      <div className="recsResult_map">
-        <NaverMap recommendations={recommendations} />
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
